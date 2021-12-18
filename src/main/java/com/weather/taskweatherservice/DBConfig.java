8ef970a8b6bfcb44731d16c70d6f0084d957d735
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-12-19 01:31:17 +0300

    fix connection on db

--- src/main/java/com/weather/taskweatherservice/DBConfig.java
+++ src/main/java/com/weather/taskweatherservice/DBConfig.java
@@ -1,7 +1,9 @@
 package com.weather.taskweatherservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -9,13 +11,16 @@ import javax.sql.DataSource;
 @Configuration
 public class DBConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource db = new DriverManagerDataSource();
-        db.setDriverClassName("org.postgresql.Driver");
-        db.setUrl("jdbc:postgresql://localhost:5432/weather_history");
-        db.setUsername("test");
-        db.setPassword("test");
+        db.setDriverClassName(env.getProperty("driver"));
+        db.setUrl(env.getProperty("url"));
+        db.setUsername(env.getProperty("name"));
+        db.setPassword(env.getProperty("password"));
         return db;
     }
 }
